998a91b054ea16dba379e244d2c8d6cea587f613
VuTrungNghia <vunghia467@gmail.com>
2024-08-11 15:29:54 +0700

    updateHomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,34 +1,20 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 const styles = {
     container: {
         display: 'flex',
         flexDirection: 'column',
-        height: '100vh',
-        backgroundColor: '#f0f2f5',
-        padding: '0 20px',
-        boxSizing: 'border-box',
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        height: '500px', 
+        fontFamily: 'Arial, sans-serif', 
     },
-    header: {
-        backgroundColor: '#4CAF50',
-        color: 'white',
-        padding: '10px 20px',
-        textAlign: 'center',
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    },
-    mainContent: {
-        flex: 1,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    footer: {
-        backgroundColor: '#333',
-        color: 'white',
-        textAlign: 'center',
-        padding: '10px',
-        fontSize: '0.875rem',
-        width: '100%',
+    h1: {
+        fontSize: '30px', 
+        color: '#FFFFFF', 
+        background: '#33CCFF', 
+        padding: '20px', 
+        borderRadius: '8px', 
     },
     chatbot: {
         width: '100%',
@@ -38,17 +24,20 @@ const styles = {
 };
 
 const HomePage = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <div style={styles.container}>
-            <header style={styles.header}>
-                <h1>Vu Trung Nghia</h1>
-            </header>
-            <main style={styles.mainContent}>
-                <h2>Welcome Chat Bot Website</h2>
-            </main>
-            <footer style={styles.footer}>
-                <p>&copy; 2024 By Vu Trung Nghia.</p>
-            </footer>
+            <h1 style={styles.h1}>Vu Trung Nghia</h1>
         </div>
     );
 };
