1162c83c46e01efe25759d7e1f504cc06121749b
VuTrungNghia <vunghia467@gmail.com>
2024-08-11 13:26:43 +0700

    createProject

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'; 
+
+function App() {
+
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+export default App
